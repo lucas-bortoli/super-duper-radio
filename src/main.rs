ed1fb61cdb5fa29886eb7e1eac26498ac66b59bd
HuanRluchetti <huan.luchetti@gmail.com>
2025-04-03 21:35:29 -0300

    solve-issue: criar docker da aplicação

--- src/main.rs
+++ src/main.rs
@@ -33,20 +33,45 @@ extern crate rocket;
 //     sender: broadcast::Sender<Box<[u8; POLL_BUFFER_SIZE_BYTES]>>,
 // }
 
+#[get("/get_stations")]
+fn get_stations() -> &'static str {
+    "Rentorna a lista de estacoes ativas no momento!" 
+}
 
+#[post("/like/<station>")]
+fn like(station: &str) -> (){
+    // adiciona o like na track
+}
+
+#[get("/station/<station>")]
+fn station(station: &str) -> &'static str {
+ ""
+}
+
+// #[post("/send_message", data = "<message>")]
+// fn send_message(message: Json<String>) -> String {
+//     format!("message: {}", message.0)
+// }
+
+// #[post("/play")]
+// fn play_music(state: &State<AudioBroadcaster>) -> &'static str {
+//     "Reproduzindo música!"
+// }
 
 #[launch]
 fn rocket() -> _ {
-    //let (tx, _) = broadcast::channel::<Box<[u8; POLL_BUFFER_SIZE_BYTES]>>(8);
+    // let (tx, _) = broadcast::channel::<Box<[u8; POLL_BUFFER_SIZE_BYTES]>>(8);
 
-    //let broadcaster = Arc::new(AudioBroadcaster { sender: tx.clone() });
+    // let broadcaster = Arc::new(AudioBroadcaster { sender: tx.clone() });
 
     // let station = Station::from_file("./diamond_city_radio/radio.yaml")
-    //     .expect("Failed to parse station file");
+    // .expect("Failed to parse station file");
     // let station_thread_clone = station.clone();
 
+    let station ="teste";
+
     rocket::build()
-        //.manage(broadcaster)
-        //.mount("/", routes![index, stylesheet])
-        //.mount("/station", routes![station_diamondcity])
+        // .manage(broadcaster)
+        // .mount("/", routes![index, stylesheet])
+        .mount("/station", routes![station])
 }
