7d9b7fb5fca4024f4f02c019e5b994427da11439
HuanRluchetti <huan.luchetti@gmail.com>
2025-04-10 19:43:10 -0300

    add: front-end files and route

--- src/main.rs
+++ src/main.rs
@@ -19,44 +19,20 @@ use std::{
 #[macro_use]
 extern crate rocket;
 
-// #[get("/")]
-// fn index() -> RawHtml<&'static [u8]> {
-//     return RawHtml(include_bytes!("ui.html"));
-// }
+#[get("/")]
+fn index() -> RawHtml<&'static [u8]> {
+    return RawHtml(include_bytes!("ui/ui.html"));
+}
 
-// #[get("/ui.css")]
-// fn stylesheet() -> RawCss<&'static [u8]> {
-//     return RawCss(include_bytes!("ui.css"));
-// }
-
-// struct AudioBroadcaster {
-//     sender: broadcast::Sender<Box<[u8; POLL_BUFFER_SIZE_BYTES]>>,
-// }
+#[get("/ui.css")]
+fn stylesheet() -> RawCss<&'static [u8]> {
+    return RawCss(include_bytes!("ui/ui.css"));
+}
 
 #[get("/get_stations")]
 fn get_stations() -> &'static str {
     "Rentorna a lista de estacoes ativas no momento!" 
 }
-
-#[post("/like/<station>")]
-fn like(station: &str) -> (){
-    // adiciona o like na track
-}
-
-#[get("/station/<station>")]
-fn station(station: &str) -> &'static str {
- ""
-}
-
-// #[post("/send_message", data = "<message>")]
-// fn send_message(message: Json<String>) -> String {
-//     format!("message: {}", message.0)
-// }
-
-// #[post("/play")]
-// fn play_music(state: &State<AudioBroadcaster>) -> &'static str {
-//     "Reproduzindo música!"
-// }
 
 #[launch]
 fn rocket() -> _ {
@@ -71,9 +47,8 @@ fn rocket() -> _ {
     let station ="teste";
 
     rocket::build()
-        // .manage(broadcaster)
-        // .mount("/", routes![index, stylesheet])
-        .mount("/station", routes![station])
+        .mount("/", routes![index, stylesheet])     
+        .mount("/", routes![])
 }
 
 // sobe
