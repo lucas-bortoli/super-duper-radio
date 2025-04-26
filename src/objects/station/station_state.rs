033ee9a810fb27eaf2d9c614142af654adaa9168
angeloavelinoo <angeloavelino33211781@gmail.com>
2025-04-26 14:19:49 -0300

    refactor: ajusta station_state

--- src/objects/station/station_state.rs
+++ src/objects/station/station_state.rs
@@ -1,16 +1,95 @@
-// estrutura de dados para armazenar o estado da estação
+use crate::objects::station::station::Station;
+
 pub trait StationState {
-
+    fn play(self: Box<Self>, station: &mut Station) -> Box<dyn StationState>;
+    fn stop(self: Box<Self>, station: &mut Station) -> Box<dyn StationState>;
+    fn next(self: Box<Self>, station: &mut Station) -> Box<dyn StationState>;
+    fn name(&self) -> &'static str;
 }
 
-pub struct MockStationState {
-    pub teste: i64,
+pub struct DownState;
+impl DownState {
+    pub fn new() -> Self { DownState }
 }
-
-impl MockStationState {
-    pub fn new() -> MockStationState {
-        MockStationState { teste: 1 }
+impl StationState for DownState {
+    fn play(self: Box<Self>, station: &mut Station) -> Box<dyn StationState> {
+        station.start_playback();
+        Box::new(PlayingState::new())
     }
+    fn stop(self: Box<Self>, _station: &mut Station) -> Box<dyn StationState> {
+        self
+    }
+    fn next(self: Box<Self>, _station: &mut Station) -> Box<dyn StationState> {
+        self
+    }
+    fn name(&self) -> &'static str { "Down" }
 }
 
-impl StationState for MockStationState {}
+pub struct PlayingState;
+impl PlayingState {
+    pub fn new() -> Self { PlayingState }
+}
+impl StationState for PlayingState {
+    fn play(self: Box<Self>, _station: &mut Station) -> Box<dyn StationState> {
+        self
+    }
+    fn stop(self: Box<Self>, station: &mut Station) -> Box<dyn StationState> {
+        station.stop_playback();
+        Box::new(DownState::new())
+    }
+    fn next(self: Box<Self>, station: &mut Station) -> Box<dyn StationState> {
+        station.save_snapshot();
+        if let Ok(next_track) = station.iterator.get_next() {
+            station.current_track = next_track;
+            station.notify_track_change();
+            Box::new(PlayingState::new())
+        } else {
+            station.stop_playback();
+            Box::new(DownState::new())
+        }
+    }
+    fn name(&self) -> &'static str { "Playing" }
+}
+
+pub struct PlayingSilentlyState;
+impl PlayingSilentlyState {
+    pub fn new() -> Self { PlayingSilentlyState }
+}
+impl StationState for PlayingSilentlyState {
+    fn play(self: Box<Self>, station: &mut Station) -> Box<dyn StationState> {
+        Box::new(PlayingState::new())
+    }
+    fn stop(self: Box<Self>, station: &mut Station) -> Box<dyn StationState> {
+        station.stop_playback();
+        Box::new(DownState::new())
+    }
+    fn next(self: Box<Self>, station: &mut Station) -> Box<dyn StationState> {
+        station.save_snapshot();
+        if let Ok(next_track) = station.iterator.get_next() {
+            station.current_track = next_track;
+            Box::new(PlayingSilentlyState::new())
+        } else {
+            station.stop_playback();
+            Box::new(DownState::new())
+        }
+    }
+    fn name(&self) -> &'static str { "PlayingSilently" }
+}
+
+pub struct SwitchingState;
+impl SwitchingState {
+    pub fn new() -> Self { SwitchingState }
+}
+impl StationState for SwitchingState {
+    fn play(self: Box<Self>, _station: &mut Station) -> Box<dyn StationState> {
+        self
+    }
+    fn stop(self: Box<Self>, station: &mut Station) -> Box<dyn StationState> {
+        station.stop_playback();
+        Box::new(DownState::new())
+    }
+    fn next(self: Box<Self>, station: &mut Station) -> Box<dyn StationState> {
+        Box::new(PlayingState::new())
+    }
+    fn name(&self) -> &'static str { "Switching" }
+}
