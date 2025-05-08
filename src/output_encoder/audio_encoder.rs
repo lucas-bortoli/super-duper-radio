26bdd4e303cf8278c2b3c156cf12de5110bfeeb6
Lucas Vinicius de Bortoli Santos <lucasbortolisantos@gmail.com>
2025-05-08 19:56:22 -0300

    fix: audio_encoder: drop support for ogg and opus

--- src/output_encoder/audio_encoder.rs
+++ src/output_encoder/audio_encoder.rs
@@ -6,60 +6,15 @@ use std::{
     thread,
 };
 
-use crate::{input_decoder::input_audio_file::AudioPacket, output_stream::OutputStream};
-
-pub const INPUT_CHANNEL_COUNT: u32 = 2;
-pub const INPUT_SAMPLE_RATE: u32 = 44100;
-pub const INPUT_BYTE_DEPTH: u32 = 2; //16bits
-
-pub const OUTPUT_CHANNEL_COUNT: u32 = 2;
-pub const OUTPUT_SAMPLE_RATE: u32 = 44100;
-pub const OUTPUT_BYTE_DEPTH: u32 = 2; //16bits
-
-pub const MAX_STATION_LISTENERS: usize = 64; // quantos players podem ouvir essa estação de uma vez?
+use crate::{
+    input_decoder::{self, AudioPacket},
+    output_stream::OutputStream,
+};
 
 #[derive(PartialEq, Eq, Hash, Clone)]
 pub enum OutputCodec {
     Mp3_64kbps,
-    Ogg96kbps,
-    Opus128kbps,
-}
-
-fn ffmpeg_args(output_codec: &OutputCodec) -> Vec<String> {
-    let sample_rate = INPUT_SAMPLE_RATE.to_string();
-    let channel_count = INPUT_CHANNEL_COUNT.to_string();
-
-    let mut args = vec![
-        "-f",
-        "s16le",
-        "-ar",
-        &sample_rate,
-        "-ac",
-        &channel_count,
-        "-i",
-        "-", // stdin como input pro ffmpeg
-    ];
-
-    args.append(&mut match output_codec {
-        OutputCodec::Mp3_64kbps => vec![
-            "-b:a",
-            "128k",
-            "-f",
-            "mp3",
-            "-flush_packets",
-            "1",
-            "-write_xing",
-            "0",
-            "-id3v2_version",
-            "0",
-        ],
-        OutputCodec::Ogg96kbps => vec!["-b:a", "96k", "-f", "ogg"],
-        OutputCodec::Opus128kbps => vec!["-c:a", "libopus", "-b:a", "128k", "-f", "opus"],
-    });
-
-    args.push("-"); // stdout como output pro ffmpeg
-
-    return args.iter().map(|f| f.to_string()).collect();
+    Mp3_128kbps,
 }
 
 pub type ConsumerPacket = Bytes;
@@ -72,9 +27,9 @@ pub struct AudioEncoder {
 
 impl AudioEncoder {
     pub fn new(output_codec: &OutputCodec, output: Arc<OutputStream>) -> AudioEncoder {
-        let args: Vec<String> = ffmpeg_args(&output_codec);
+        let args: Vec<String> = AudioEncoder::ffmpeg_args(&output_codec);
 
-        println!("encoder: Parâmetros ffmpeg: {:?}", args);
+        println!("encoder: parâmetros ffmpeg: {:?}", args);
 
         let mut child = Command::new("ffmpeg")
             .args(args)
@@ -87,16 +42,16 @@ impl AudioEncoder {
         if let Some(stdout) = child.stdout.take() {
             let mut stdout_reader = BufReader::new(stdout);
             thread::spawn(move || {
-                println!("encoder: Thread de consumidor de áudio iniciada.");
+                println!("encoder: thread de consumidor de áudio iniciada.");
 
                 let mut buf = vec![0u8; 8192];
                 loop {
                     let n = stdout_reader
                         .read(&mut buf)
-                        .expect("encoder: Ler stdout do encoder falhou - processo crashou?");
+                        .expect("encoder: ler stdout do encoder falhou - processo crashou?");
 
                     match n {
-                        0 => panic!("encoder: Stdout finalizou, estação acabou!"),
+                        0 => panic!("encoder: stdout finalizou, estação acabou!"),
                         1.. => {
                             // não é exatamente zero-copy, mas sim "one-copy"
                             // uma vez que alocamos esse Bytes, ele é reference-counted, igual o Arc
@@ -111,7 +66,7 @@ impl AudioEncoder {
             });
         }
 
-        let stdin = child.stdin.take().expect("encoder: Falha ao ler stdin");
+        let stdin = child.stdin.take().expect("encoder: falha ao ler stdin");
         let stdin_writer = BufWriter::new(stdin);
 
         AudioEncoder {
@@ -123,11 +78,58 @@ impl AudioEncoder {
     pub fn push_audio_packet(&mut self, packet: AudioPacket) {
         self.encoder_in
             .write(&packet.buffer)
-            .expect("encoder: A fila do ffmpeg está cheia?");
+            .expect("encoder: a fila do ffmpeg está cheia?");
 
         // bypass do buffer do stdin; manda direto pro ffmpeg, já que áudio é em real-time e talvez não seja legal ter esse comportamento de buffering
         // ignoramos o Result propositalmente, não há nenhuma ação cabível a ser tomada se o buffer de stdin não pode ser flushado - meio que não importa
         let _ = self.encoder_in.flush();
+    }
+
+    fn ffmpeg_args(output_codec: &OutputCodec) -> Vec<String> {
+        let sample_rate = input_decoder::SAMPLE_RATE.to_string();
+        let channel_count = input_decoder::CHANNEL_COUNT.to_string();
+
+        let mut args = vec![
+            "-f",
+            "s16le",
+            "-ar",
+            &sample_rate,
+            "-ac",
+            &channel_count,
+            "-i",
+            "-", // stdin como input pro ffmpeg
+        ];
+
+        args.append(&mut match output_codec {
+            OutputCodec::Mp3_64kbps => vec![
+                "-b:a",
+                "64k",
+                "-f",
+                "mp3",
+                "-flush_packets",
+                "1",
+                "-write_xing",
+                "0",
+                "-id3v2_version",
+                "0",
+            ],
+            OutputCodec::Mp3_128kbps => vec![
+                "-b:a",
+                "128k",
+                "-f",
+                "mp3",
+                "-flush_packets",
+                "1",
+                "-write_xing",
+                "0",
+                "-id3v2_version",
+                "0",
+            ],
+        });
+
+        args.push("-"); // stdout como output pro ffmpeg
+
+        return args.iter().map(|f| f.to_string()).collect();
     }
 }
 
@@ -135,6 +137,6 @@ impl Drop for AudioEncoder {
     fn drop(&mut self) {
         self.child
             .kill()
-            .expect("encoder: ffmpeg não pôde ser fechado");
+            .expect("encoder: o ffmpeg não pôde ser fechado");
     }
 }
--- src/output_encoder/null_frames/mod.rs
+++ src/output_encoder/null_frames/mod.rs
@@ -16,22 +16,16 @@ use super::audio_encoder::OutputCodec;
 /// sobre o início da stream.
 ///
 /// **Tradeoff**: O cliente recebe um pacote de 0.02s de áudio silencioso no começo da stream.
-///
-/// `ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -acodec libmp3lame -ab 128k -ac 2 -ar 44100 -t 0.02 -vn -sn -f mp3 - | head -c 1472 > mp3_null_frame.bin`
-/// `ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=48000 -acodec libopus -ab 128k -ac 2 -ar 48000 -t 0.02 -vn -sn -f opus - > opus_null_frame.opus`
-/// `ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -acodec libvorbis -ab 128k -ac 2 -ar 44100 -t 0.02 -vn -sn -f ogg - > ogg_vorbis_null_frame.ogg`
 pub fn get_null_frame(codec: &OutputCodec) -> &'static [u8] {
     match codec {
         OutputCodec::Mp3_64kbps => include_bytes!("./mp3.bin"),
-        OutputCodec::Ogg96kbps => include_bytes!("./ogg.bin"),
-        OutputCodec::Opus128kbps => include_bytes!("./opus.bin"),
+        OutputCodec::Mp3_128kbps => include_bytes!("./mp3.bin"),
     }
 }
 
 pub fn get_mime_type(codec: &OutputCodec) -> &'static str {
     match codec {
         OutputCodec::Mp3_64kbps => "mpeg",
-        OutputCodec::Ogg96kbps => "ogg",
-        OutputCodec::Opus128kbps => "opus",
+        OutputCodec::Mp3_128kbps => "mpeg",
     }
 }
