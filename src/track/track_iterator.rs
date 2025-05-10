bcd3ebe64307f5ec5b84251de4682203804b4cd2
Lucas Vinicius de Bortoli Santos <lucasbortolisantos@gmail.com>
2025-05-10 14:05:58 -0300

    fix: track_iterator not yielding tracks

--- src/track/track_iterator.rs
+++ src/track/track_iterator.rs
@@ -1,24 +1,20 @@
 use super::track::Track;
 use frand::{Rand, Shufflable};
-use std::collections::HashSet;
+use std::collections::VecDeque;
 
 /// Iterador que reproduz trilhas de uma lista de forma aleatória, sem repetição dentro de cada ciclo.
 /// O iterador garante que cada trilha seja reproduzida apenas uma vez por ciclo, e ao final do ciclo,
 /// as trilhas são reembaralhadas para começar novamente.
 pub struct TrackIterator {
     tracks: Vec<Track>,
-    indices: Vec<usize>,
-    used_indices: HashSet<usize>,
+    track_queue: VecDeque<Track>,
 }
 
 impl TrackIterator {
     pub fn new(all_tracks: Vec<Track>) -> Self {
-        let indices: Vec<usize> = (0..all_tracks.len()).collect();
-
         TrackIterator {
             tracks: all_tracks,
-            indices,
-            used_indices: HashSet::new(),
+            track_queue: VecDeque::new(),
         }
     }
 
@@ -28,24 +24,11 @@ impl TrackIterator {
     /// automaticamente após todas terem sido reproduzidas.
     pub fn next(&mut self, rng: &mut Rand) -> Option<Track> {
         // se nenhum índice estiver disponível, reembaralhar e zerar a lista de usados
-        if self.used_indices.is_empty() {
-            self.indices.shuffle(rng);
-            self.used_indices.clear();
+        if self.track_queue.is_empty() {
+            // repopular a fila de tracks, com ordem aleatória
+            self.track_queue = VecDeque::from(self.tracks.clone().shuffled(rng));
         }
 
-        // encontrar um índice não usado
-        let mut picked_idx = None;
-        for &idx in &self.indices {
-            if !self.used_indices.contains(&idx) {
-                picked_idx = Some(idx);
-                break;
-            }
-        }
-
-        // se nenhum índice não usado for encontrado (não deve acontecer), retornar None
-        let picked_idx = picked_idx?;
-        self.used_indices.insert(picked_idx);
-
-        Some(self.tracks[picked_idx].clone())
+        self.track_queue.pop_front()
     }
 }
